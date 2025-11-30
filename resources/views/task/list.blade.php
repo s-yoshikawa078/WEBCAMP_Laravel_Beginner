9961c448e8abf532129d908ed5a8121d35517173
yoshikawa <s-yoshikawa078@users.noreply.github.com>
2025-11-30 04:55:44 +0000

    ページネーションを書いた

--- resources/views/task/list.blade.php
+++ resources/views/task/list.blade.php
@@ -33,7 +33,7 @@
         <tr>
             <th>タスク名
             <th>期限
-            <th>重要度        
+            <th>重要度
         @foreach ($list as $task)
         <tr>
             <td>{{ $task->name }}
@@ -45,10 +45,25 @@
         @endforeach
         </table>
         <!-- ページネーション -->
-        現在 1 ページ目<br>
-        <a href="./top.html">最初のページ(未実装)</a> / 
-        <a href="./top.html">前に戻る(未実装)</a> / 
-        <a href="./top.html">次に進む(未実装)</a>
+        現在 {{ $list->currentPage() }} ページ目<br>
+        {{-- {{ $list->links() }} --}}
+        @if ($list->onFirstPage() === false)
+        <a href="/task/list">最初のページ</a>
+        @else
+        最初のページ
+        @endif
+        / 
+        @if ($list->previousPageUrl() !== null)
+            <a href="{{ $list->previousPageUrl() }}">前に戻る</a>
+        @else
+            前に戻る
+        @endif
+        / 
+        @if ($list->nextPageUrl() !== null)
+            <a href="{{ $list->nextPageUrl() }}">次に進む</a>
+        @else
+            次に進む
+        @endif
         <br>
         <hr>
         <menu label="リンク">
